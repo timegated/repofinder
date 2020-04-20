2ab30852c6e4b64a2e5646c66aa64f9fe4431265
dbeccaria826 <beccdaniel@gmail.com>
2020-04-20 13:17:37 -0400

    working webpack+express config, getting weird memory-file-system error but page still loads

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import logMessage from './js/logger'
+import './css/style.css'
+
+// Log message to console
+logMessage('Its finished!!')
+
+if (module.hot) // eslint-disable-line no-undef
+    module.hot.accept() // eslint-disable-line no-undef
