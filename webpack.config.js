b6dbab6b845f577c40f767278350a08d495a72c1
dbeccaria826 <beccdaniel@gmail.com>
2020-04-20 12:11:43 -0400

    added working webpack set up

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const webpackMerge = require('webpack-merge');
+
+const modeConfig = env => require(`./build-utils/webpack.${env}.js`)(env);
+
+const presetConfig = require('./build-utils/loadPresets');
+module.exports = ({
+    mode,
+    presets
+} = {
+    mode: "production",
+    presets: []
+}) => {
+    return webpackMerge({
+            mode,
+            output: {
+                filename: 'bundle.js'
+            },
+            plugins: [new HtmlWebPackPlugin(), new webpack.ProgressPlugin()]
+        },
+        modeConfig(mode),
+        presetConfig({
+            mode,
+            presets
+        })
+    );
+};
